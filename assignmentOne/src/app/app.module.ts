81c323089c5363be9e8b152a4b53373d95f33a9f
darshanlal2k <darshanlal2k@gmail.com>
2023-11-08 15:32:07 +0530

    add

---
+++ assignmentOne/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { successAlertComponent } from './success-alert/success-alert-component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    successAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
