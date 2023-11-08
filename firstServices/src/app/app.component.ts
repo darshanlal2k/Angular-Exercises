0eb264741845f63d3e5183c0f1f9a4b1b3acce31
darshanlal2k <darshanlal2k@gmail.com>
2023-11-08 16:04:06 +0530

    16 services

---
+++ firstServices/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { EventService } from './services/event.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstServices';
+  constructor(public eventService: EventService) {
+
+  }
+  events = this.eventService.getEvents();
+}
